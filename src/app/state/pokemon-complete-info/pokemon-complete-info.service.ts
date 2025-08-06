6971b605eabfd234955846358e63742aa5e846ed
Allison Barros <bx-guy@bx-guy>
2025-08-06 01:45:07 -0400

    changed the pokemon details result to a forkJoin to fetch everything at the same time

--- src/app/state/pokemon-complete-info/pokemon-complete-info.service.ts
+++ src/app/state/pokemon-complete-info/pokemon-complete-info.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { forkJoin, from } from 'rxjs';
 import { concatMap, map, switchMap, tap, toArray } from 'rxjs/operators';
-import { PokemonCompleteInfoStore } from './pokemon-complete-info.store';
 import { TypeListPokemons } from '../../models/pokemon.model';
 import { PokemonCompleteInfo } from './pokemon-complete-info.model';
-import { from } from 'rxjs';
 import { PokemonCompleteInfoQuery } from './pokemon-complete-info.query';
+import { PokemonCompleteInfoStore } from './pokemon-complete-info.store';
 
 @Injectable({ providedIn: 'root' })
 export class PokemonCompleteInfoService {
@@ -36,24 +36,23 @@ export class PokemonCompleteInfoService {
     return this._query.isEmptyByType$(type).pipe(
       tap(() => this.pokemonCompleteInfoStore.setLoading(true)),
       switchMap((isEmpty) => {
-        if (isEmpty) {
-          return this._httpClient.get<TypeListPokemons>(`type/${type}`).pipe(
-            map((pokemonRes) => pokemonRes.pokemon),
-            switchMap((pokemons) =>
-              from(pokemons).pipe(
-                concatMap((p) =>
-                  this._httpClient
-                    .get<PokemonCompleteInfo>(`pokemon/${p.pokemon.name}`)
-                    .pipe(map((res) => ({ ...res, type })))
-                ),
-                toArray()
+        if (!isEmpty) return this._query.getListByType$(type);
+        //  Get basic Pokémon list for the type
+        return this._httpClient.get<TypeListPokemons>(`type/${type}`).pipe(
+          map((res) => res.pokemon.map((p) => p.pokemon.name)),
+          // Fetch all complete Pokémon info in parallel
+          switchMap((names) =>
+            forkJoin(
+              names.map((name) =>
+                this._httpClient
+                  .get<PokemonCompleteInfo>(`pokemon/${name}`)
+                  .pipe(map((res) => ({ ...res, type })))
               )
-            ),
-            tap((results) => this.pokemonCompleteInfoStore.add(results))
-          );
-        } else {
-          return this._query.getListByType$(type);
-        }
+            )
+          ),
+          // Save in store
+          tap((results) => this.pokemonCompleteInfoStore.add(results))
+        );
       }),
       tap(() => this.pokemonCompleteInfoStore.setLoading(false))
     );
